03ef7f5350354d6a3d70b8667a6b1303984e0dbd
DongkyuEunyeon <dkjeong.joel@kakao.com>
2026-03-12 23:16:12 +0900

    draft

--- src/component/invitation/index.tsx
+++ src/component/invitation/index.tsx
@@ -25,17 +25,19 @@ export const Invitation = () => {
 
       <div className="break" />
 
-      <div className="content">싱그러운 여름 향기 가득한 날</div>
-      <div className="content">소중한 분들을 모시고</div>
-      <div className="content">사랑의 약속을 하려고 합니다.</div>
-      <div className="break" />
-      <div className="content">햇살이 뜨거울 땐 가려주고,</div>
-      <div className="content">비가 오면 우산이 되어주는</div>
-      <div className="content">부부가 되겠습니다.</div>
-      <div className="break" />
-      <div className="content">기쁜날 함께 하시어</div>
-      <div className="content">저희의 앞날을 축복해 주세요.</div>
+      <div className="content">무엇보다도 서로 깊이 사랑하십시오.</div>
+      <div className="content">사랑은 허다한 죄를 덮습니다.</div>
+      <div className="content">베드로전서 4:8</div>
 
+      <div className="break" />
+      <div className="break" />
+
+      <div className="content">이 말씀 안에서</div>
+      <div className="content">사랑으로 서로를 덮는 작은 교회로</div>
+      <div className="content">한 가정을 세워가려 합니다.</div>
+      <div className="content">귀한 걸음으로 함께 축복해 주세요.</div>
+      
+      <div className="break" />
       <div className="break" />
 
       <div className="name">
